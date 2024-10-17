9b92f356d1ef10d7bb3ff4d725899195493b1595
yamashitafumihiro <fumiwata2000@gmail.com>
2024-10-17 14:43:43 +0900

    made graph generator and db connector

--- graph/graph-generation.go
+++ graph/graph-generation.go
@@ -1,1 +1,52 @@
 package graph
+
+import (
+	"context"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"log"
+)
+
+type Generator struct {
+	driver neo4j.DriverWithContext
+}
+
+func NewGenerator(driver neo4j.DriverWithContext) *Generator {
+	return &Generator{driver: driver}
+}
+
+func (generator *Generator) CreateGraph(ctx context.Context, nodeCount, edgeCount int) (int, error) {
+	session := generator.driver.NewSession(ctx, neo4j.SessionConfig{})
+	defer func(session neo4j.SessionWithContext, ctx context.Context) {
+		err := session.Close(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(session, ctx)
+
+	result, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (interface{}, error) {
+		return generator.createGraphTx(ctx, tx, nodeCount, edgeCount)
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return result.(int), nil
+}
+
+func (generator *Generator) createGraphTx(ctx context.Context, tx neo4j.ManagedTransaction, nodeCount, edgeCount int) (int, error) {
+	_, err := tx.Run(ctx, "cypher query", map[string]interface{}{"count": nodeCount})
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = tx.Run(ctx, `
+		cypher query
+	`, map[string]interface{}{"edgeCount": edgeCount})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return nodeCount, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"log"
+	"random-graph-generation-on-neo4j/graph"
+)
 
+func main() {
+	ctx := context.Background()
+
+	dbUri := "neo4j://localhost:7687"
+
+	driver, err := neo4j.NewDriverWithContext(dbUri, neo4j.NoAuth())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(driver neo4j.DriverWithContext, ctx context.Context) {
+		err := driver.Close(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(driver, ctx)
+
+	err = driver.VerifyConnectivity(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection established.")
+
+	generator := graph.NewGenerator(driver)
+
+	nodeCount := 5
+	edgeCount := 5
+
+	createdNodes, err := generator.CreateGraph(ctx, nodeCount, edgeCount)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Created graph with %d nodes and %d edges\n", createdNodes, edgeCount)
 }
